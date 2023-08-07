49ee0ed13829bc1b21047bfe1cb0e7328125b4d8
osmangony01 <mdosmangoni376@gmail.com>
2023-08-07 11:33:55 +0600

    add about section in home page

---
+++ src/components/Home/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="my-32">
+            <div className='w-full px-6 md:px-0 md:w-4/5 lg:w-3/4 mx-auto'>
+                <h2 className='text-3xl font-bold text-center pb-4'>What People Say About WorkSpeak</h2>
+                <p className='text-center'>With the open university, you can study whenever and wherever you choose. We have students in over 128 countries and a global reputation as a pioneer in the field of flexible learning. Our flexible teaching also means, if you travel often or need to relocate, you can continue to study wherever you go.</p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
